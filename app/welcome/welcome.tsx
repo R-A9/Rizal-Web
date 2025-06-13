9a870c4516bf3826d844e7b484079f80f6865b01
Sakamoto <giulliandawal@gmail.com>
2025-06-13 18:12:00 +0800

    Work dropdown removed

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { TypeAnimation } from 'react-type-animation';
 import { Menu, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 function classNames(...classes: string[]) {
@@ -9,6 +9,10 @@ function classNames(...classes: string[]) {
 }
 
 export function Welcome() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       {/* Header */}
@@ -22,54 +26,7 @@ export function Welcome() {
           </div>
           <nav className="hidden md:flex space-x-4">
             <Link to="/" className="text-gray-900 hover:text-amber-600 font-medium px-3 py-2">Home</Link>
-            
-            {/* Works Dropdown */}
-            <Menu as="div" className="relative">
-              <Menu.Button className="text-gray-500 hover:text-amber-600 font-medium px-3 py-2 flex items-center">
-                Works
-                <ChevronDownIcon className="ml-1 h-5 w-5" aria-hidden="true" />
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to="/works/letters"
-                          className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
-                          )}
-                        >
-                          Letters of Rizal
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to="/works/poems"
-                          className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
-                          )}
-                        >
-                          Poems of Rizal
-                        </Link>
-                      )}
-                    </Menu.Item>
-                  </div>
-                </Menu.Items>
-              </Transition>
-            </Menu>
+            <Link to="/works" className="text-gray-500 hover:text-amber-600 font-medium px-3 py-2">Works</Link>
 
             {/* Novels Dropdown */}
             <Menu as="div" className="relative">
