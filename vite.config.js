7bfd92a846b75219002f23fcaebfbfd2bd3bf68f
Sakamoto <giulliandawal@gmail.com>
2025-06-15 15:53:56 +0800

    fixed2

--- vite.config.js
+++ vite.config.js
@@ -16,18 +16,9 @@ export default defineConfig({
   
   build: {
     target: 'es2022',
-    // Reduce bundle size for serverless
     reportCompressedSize: false,
     cssMinify: true,
-    sourcemap: false,
-    // Optimize for serverless
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    sourcemap: false
   },
   
   // Enable asset optimization
@@ -35,13 +26,6 @@ export default defineConfig({
   
   // Server configuration for development
   server: {
-    https: false,
-    // Reduce memory usage
-    preTransformRequests: false
-  },
-  
-  // Reduce memory usage
-  esbuild: {
-    logLevel: 'error'
+    https: false
   }
 });
